6e55b6bdb34471f4f2a25540c18325829a1d79e2
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-01 19:59:50 +0200

    fixed api key error

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path:'/.env'})
+require('dotenv').config({path:'.env'})
 const express = require('express');
 const axios = require('axios');
 
@@ -6,8 +6,15 @@ const app = express();
 
 const API_KEY = process.env['API_TOKEN'];
 
-app.get('/contacts', (req, res) => {
-
+app.get('/contacts', async (req, res) => {
+    const contacts = `https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=${API_KEY}`;
+    try{
+        const resp = await axios.get(contacts);
+        const data = resp.data;
+        res.json(data);
+    }catch(err){
+        console.error(err)
+    }
 });
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'))
