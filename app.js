04fe305dbfe283d83fd786f3a292c9c41b13f9a8
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-02 19:53:44 +0200

    added a delete request

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { request } = require('express');
 const app = express();
 
 const API_KEY = process.env['API_TOKEN'];
-const contactsToDeleteById = [];
+let contactsToDeleteById = [];
 
 app.get('/api/contacts', async (req, res) => {
     const contacts = `https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=${API_KEY}`;
@@ -24,13 +24,6 @@ app.get('/api/contacts', async (req, res) => {
         deleteContacts.map(contact =>{
             contactsToDeleteById.push(contact.vid)
         })
-        
-        //   contactEmails.push(data.contacts;
-        // for( let i = 0; i <  filteredContactsToDelete.length; i++){
-        //     const contactId = filteredContactsToDelete[i].vid;
-        //     const deleteContactByIdUrl = `https://api.hubapi.com/contacts/v1/contact/vid/${contactId}?hapikey=${API_KEY}`
-        //     console.log(deleteContactByIdUrl)
-        // }
     }catch(err){
         console.error(err)
     }
@@ -39,9 +32,23 @@ app.get('/api/contacts', async (req, res) => {
  app.delete('/api/contacts/:id', (req, res) => {
     const id = req.params.id;
 
-    const deleted = contactsToDeleteById.find(contact => contact.id === id)
+
+    const deleted = contactsToDeleteById.find(contact => contact.vid === id)
+    res.status(200).json(deleted)
+   
     if(deleted){
-        
+        console.log(deleted)
+        contactsToDeleteById = contactsToDeleteById.filter(contact => contact.vid != id)
+    }else{
+        res.status(404).json({message: "Contact does not exist."})
     }
 })
 app.listen(3000, () => console.log('Listening on http://localhost:3000'))
+
+
+ //   contactEmails.push(data.contacts;
+        // for( let i = 0; i <  filteredContactsToDelete.length; i++){
+        //     const contactId = filteredContactsToDelete[i].vid;
+        //     const deleteContactByIdUrl = `https://api.hubapi.com/contacts/v1/contact/vid/${contactId}?hapikey=${API_KEY}`
+        //     console.log(deleteContactByIdUrl)
+        // }
