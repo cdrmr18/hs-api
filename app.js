9e3c6f1c05f7ff54245f3c7e063a5f2c9508cf54
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-02 19:26:12 +0200

    worked on creating a simple filter for contacts & starting to write a delete method

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 require('dotenv').config({path:'.env'})
 const express = require('express');
 const axios = require('axios');
+const { request } = require('express');
 
 const app = express();
 
 const API_KEY = process.env['API_TOKEN'];
+const contactsToDeleteById = [];
 
-app.get('/contacts', async (req, res) => {
+app.get('/api/contacts', async (req, res) => {
     const contacts = `https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=${API_KEY}`;
     try{
         const resp = await axios.get(contacts);
         const data = resp.data;
-        res.json(data);
+        res.status(200).json(data);
+
+        const filteredData = data.contacts.filter(contact => {
+            const contactsToDelete = contact.vid === 1;
+            return contactsToDelete
+        })
+        
+        const deleteContacts = filteredData;
+        deleteContacts.map(contact =>{
+            contactsToDeleteById.push(contact.vid)
+        })
+        
+        //   contactEmails.push(data.contacts;
+        // for( let i = 0; i <  filteredContactsToDelete.length; i++){
+        //     const contactId = filteredContactsToDelete[i].vid;
+        //     const deleteContactByIdUrl = `https://api.hubapi.com/contacts/v1/contact/vid/${contactId}?hapikey=${API_KEY}`
+        //     console.log(deleteContactByIdUrl)
+        // }
     }catch(err){
         console.error(err)
     }
 });
 
+ app.delete('/api/contacts/:id', (req, res) => {
+    const id = req.params.id;
+
+    const deleted = contactsToDeleteById.find(contact => contact.id === id)
+    if(deleted){
+        
+    }
+})
 app.listen(3000, () => console.log('Listening on http://localhost:3000'))
