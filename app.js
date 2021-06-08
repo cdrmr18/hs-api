5f38c0fb239c76cfbcf33f781796070fa2851a5b
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-08 08:48:21 +0200

    completed rest api

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 require('dotenv').config({path:'.env'})
 const express = require('express');
 const axios = require('axios');
-const { request } = require('express');
 
 const app = express();
 
 const API_KEY = process.env['API_TOKEN'];
-let contactsToDeleteById = [];
 
-app.get('/api/contacts', async (req, res) => {
-    const contacts = `https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=${API_KEY}`;
-    try{
-        const resp = await axios.get(contacts);
-        const data = resp.data;
-        res.status(200).json(data);
+const getAllContacts = async () => {
+      //DONE:> while 'has more' is true then request the next 100 contacts based on the vid offset
+    let hasMOre = false;
+    let vidOffset = undefined;
+    let allContacts = [];
 
-        const filteredData = data.contacts.filter(contact => {
-            const contactsToDelete = contact.vid === 1;
-            return contactsToDelete
+    do {
+        const resp = await axios.get('https://api.hubapi.com/contacts/v1/lists/all/contacts/all', {
+          params: {
+              count: 100, //limits 100
+              vidOffset: vidOffset,
+              hapikey: API_KEY,
+              //property: the two props that hold deletion criteria
+          }  
         })
-        
-        const deleteContacts = filteredData;
-        deleteContacts.map(contact =>{
-            contactsToDeleteById.push(contact.vid)
+       
+        const { contacts, 'has-more': _hasMore, 'vid-offset': _vidOffset } = resp.data; //rest & spread
+        allContacts.push(...contacts);
+        hasMOre = _hasMore;
+        vidOffset = _vidOffset;
+    } while (hasMOre  === true)
+    return allContacts;
+}
+
+const deleteContact = async (contact) => {
+    await axios.delete(`https://api.hubapi.com/contacts/v1/contact/vid/${contact.vid}`, {
+          params: {
+              hapikey: API_KEY,
+          }  
         })
-    }catch(err){
-        console.error(err)
+}
+
+ app.delete('/api/inactive_contacts/', async (req, res) => {
+    const allContacts = await getAllContacts();
+    //filter sync status && last activity date
+    const filteredContacts = allContacts.filter(contact => contact.vid === 1 ) //get actual filter details 
+    //delete filtered contacts 'batch delete'
+    for (let contact of filteredContacts) {
+        await deleteContact(contact);
     }
-});
-
- app.delete('/api/contacts/:id', (req, res) => {
-    const id = req.params.id;
-
-
-    const deleted = contactsToDeleteById.find(contact => contact.vid === id)
-    res.status(200).json(deleted)
-   
-    if(deleted){
-        console.log(deleted)
-        contactsToDeleteById = contactsToDeleteById.filter(contact => contact.vid != id)
-    }else{
-        res.status(404).json({message: "Contact does not exist."})
-    }
+    res.sendStatus(200);
 })
+
 app.listen(3000, () => console.log('Listening on http://localhost:3000'))
-
-
- //   contactEmails.push(data.contacts;
-        // for( let i = 0; i <  filteredContactsToDelete.length; i++){
-        //     const contactId = filteredContactsToDelete[i].vid;
-        //     const deleteContactByIdUrl = `https://api.hubapi.com/contacts/v1/contact/vid/${contactId}?hapikey=${API_KEY}`
-        //     console.log(deleteContactByIdUrl)
-        // }
